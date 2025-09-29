b59b5480f8a760ec5f47daedbad2cf86e5ce36e8
scrubware <tomomnom99@gmail.com>
2025-09-28 19:25:47 -0600

    vite project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
-    server: {
-        port: 3000,
-        open: true
-    }
-});
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    port: 3000,
+    open: true
+  }
+})

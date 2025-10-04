8a9dce23faa58168af5a24e218289a5b11cfe34e
scrubware <tomomnom99@gmail.com>
2025-10-03 18:57:11 -0600

    fixed non-highlighting nav elements (excluding logout)

--- src/main.jsx
+++ src/main.jsx
@@ -18,13 +18,13 @@ export default function App() {
             <p className="dark:text-zinger dark:hover:text-zinger-alt text-4xl sm:text-5xl italic">zinger</p>
           </NavLink>
           <div className="flex">
-            <NavLink to="/post">
-              <svg viewBox="0 0 24 24" fill="none" className="rounded w-[12mm] h-[12mm] sm:w-[16mm] sm:h-[16mm] stroke-zinger hover:stroke-zinger-alt pointer-events-none">
+            <NavLink to="/post" className="stroke-zinger hover:stroke-zinger-alt">
+              <svg viewBox="0 0 24 24" fill="none" className="rounded w-[12mm] h-[12mm] sm:w-[16mm] sm:h-[16mm] pointer-events-none">
               <path id="Vector" d="M8 12H12M12 12H16M12 12V16M12 12V8M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
             </NavLink>
-            <NavLink to="/account">
-              <svg viewBox="0 0 24 24" fill="none" className="rounded w-[12mm] h-[12mm] sm:w-[16mm] sm:h-[16mm] stroke-zinger hover:stroke-zinger-alt pointer-events-none">
+            <NavLink to="/account" className="stroke-zinger hover:stroke-zinger-alt">
+              <svg viewBox="0 0 24 24" fill="none" className="rounded w-[12mm] h-[12mm] sm:w-[16mm] sm:h-[16mm] pointer-events-none">
               <path id="Vector" d="M17.2166 19.3323C15.9349 17.9008 14.0727 17 12 17C9.92734 17 8.06492 17.9008 6.7832 19.3323M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21ZM12 14C10.3431 14 9 12.6569 9 11C9 9.34315 10.3431 8 12 8C13.6569 8 15 9.34315 15 11C15 12.6569 13.6569 14 12 14Z" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
             </NavLink>

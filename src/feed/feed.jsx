9292dfc2805f7529936cc4a14e739e48883b426a
scrubware <tomomnom99@gmail.com>
2025-11-28 22:22:16 -0700

    third party api

--- src/feed/feed.jsx
+++ src/feed/feed.jsx
@@ -4,7 +4,31 @@ import '../main.css';
 import { OwnedPost, OwnedPostFactory, Post } from "../components/post"
 import { Ad } from "../components/ad"
 
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 export function FeedPage({feed}) {
+
+    const urlstr = () => {
+        return `https://cataas.com/cat/says/no%20posts%20yet%20%3A%2F?type=square&font=Times%20New%20Roman&fontSize=25&fontColor=%23fff&fontBackground=%23000000&t=${Date.now()}`;
+    }
+
+    const [url, setUrl] = useState(urlstr())
+
+    const location = useLocation();
+
+    useEffect(() => {
+        setUrl(urlstr());
+    }, [location]);
+
+    function GetNoPostsCat() {
+       return (
+        <div className="flex p-4 sm:p-0 items-center justify-center">
+            <img src={url} className="w-full sm:w-2/3 rounded-xl"/>
+        </div>
+       )
+    }
+
     return (
         <div className="grow">
             {/* <Post/>
@@ -14,7 +38,8 @@ export function FeedPage({feed}) {
             <Ad/>
             <br/>
             <OwnedPost/> */}
-            {feed.length > 0 ? feed.map(item => OwnedPostFactory(item)) : <p>nothing here yet... maybe you ought to make the first post?</p>}
+            {feed.length > 0 ? feed.map(item => OwnedPostFactory(item)) : GetNoPostsCat() }
+            
         </div>
     );
 }
